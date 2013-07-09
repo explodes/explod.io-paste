979c3be68485029338ff818d170edd631939aaff
Evan Leis <engineergod@yahoo.com>
2013-07-09 14:15:00 -0600

    Paste functionality and cleanup script

--- explodio/paste/managers.py
+++ explodio/paste/managers.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from explodio.common import managers
 
 
@@ -7,3 +9,11 @@ class PasteManager(managers.QuerySetManager):
 
         def by_ip_address(self, ip_address):
             return self.filter(ip_address=ip_address)
+
+        def not_expired(self):
+            now = timezone.now()
+            return self.filter(expires_at__gt=now)
+
+        def expired(self):
+            now = timezone.now()
+            return self.filter(expires_at__lte=now)
