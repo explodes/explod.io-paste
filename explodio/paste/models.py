d75720867e8a2f5206d4408698ac482b97e4127c
Evan Leis <engineergod@yahoo.com>
2013-07-26 11:06:14 -0600

    Sort paste options by visible key

--- explodio/paste/models.py
+++ explodio/paste/models.py
@@ -12,9 +12,8 @@ from explodio.paste import managers
 
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
-
+LANGUAGE_CHOICES = sorted(((item[1][0], item[0]) for item in LEXERS), key=lambda x: x[1].lower())
+STYLE_CHOICES = sorted(((item, item) for item in get_all_styles()), key=lambda x: x[1].lower())
 
 class Paste(models.Model):
 
